2049f75fd953e0c8387cd545aa48784177cd6ccb
Po-Chun Hsu <35525814+JamesGodwon@users.noreply.github.com>
2020-03-02 04:54:49 +0800

    Add files via upload

---
+++ CustMgr/客戶管理系統-更新重大訊息公告/WriteStaticHtml/WriteStaticHtml/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+
+namespace WriteStaticHtml
+{
+    class Program
+    {
+        //要另存成html的aspx
+        static string targetASPX = ConfigurationManager.AppSettings["targetASPX"];
+        //要存檔的路徑
+        static string savePath = ConfigurationManager.AppSettings["savePath"];
+        //登入帳號
+        static string account = ConfigurationManager.AppSettings["accountLogin"];
+        //登入密碼
+        static string password = ConfigurationManager.AppSettings["passwordLogin"];
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                NetworkCredential credentials = new NetworkCredential("account", account, password);
+                wc.Credentials = credentials;
+
+                //目前網頁多半是UTF8編碼
+                wc.Encoding = System.Text.Encoding.UTF8;
+                //下載html字串
+                string html = wc.DownloadString(targetASPX);
+
+
+                using (FileStream fs = new FileStream(savePath, System.IO.FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    StreamWriter sw = new StreamWriter(fs);
+                    //輸出html字串
+                    sw.Write(html);
+                    sw.Close();
+                    fs.Close();
+
+                }
+                Console.Write("OK");
+                //Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+                Console.Read();
+
+            }
+
+
+        }
+    }
+}
